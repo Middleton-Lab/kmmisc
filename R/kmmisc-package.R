5dc4ba4f5e4ce4cb091896ad243f3a4003e1ea81
Kevin Middleton <kmm@csusb.edu>
2011-08-13 21:06:37 -0400

    Initialize repository.

---
+++ R/kmmisc-package.R
@@ -0,0 +1,26 @@
+##' KM Misc
+##' 
+##' Misc. Functions
+##' 
+##' \tabular{ll}{
+##' Package: \tab kmmisc\cr
+##' Type: \tab Package\cr
+##' Version: \tab 0.1-1\cr
+##' Date: \tab 2011-08-13\cr
+##' License: \tab GPL-2\cr
+##' LazyLoad: \tab yes\cr
+##' LazyData: \tab yes\cr
+##' }
+##' 
+##' @name kmmisc-package
+##' 
+##' @docType package
+##' 
+##' @author Kevin Middleton (kmm@@csusb.edu)
+##' 
+##' Maintainer: Kevin Middleton (kmm@@csusb.edu)
+##'
+##' @keywords package
+##'
+##' 
+NULL
---
+++ README.md
@@ -0,0 +1,14 @@
+# kmmisc #
+
+An R package that contains miscellaneous functions I find useful.
+
+# Installation #
+
+You can install the most recent version directly from github using
+`install_github()` from the
+[devtools package](https://github.com/hadley/devtools).
+
+```R
+require(devtools)
+install_github("kmmisc", "kmiddleton")
+```
