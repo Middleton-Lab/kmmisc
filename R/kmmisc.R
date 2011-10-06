8f4023d65f17e24b542432d83ba3a3042a701ef3
Kevin Middleton <kmm@csusb.edu>
2011-10-06 16:20:28 -0700

    Changed minor formatting issues.

--- R/kmmisc.R
+++ R/kmmisc.R
@@ -1,26 +1,20 @@
-##' KM Misc
-##' 
-##' Misc. Functions
+##' Miscellaneous functions
 ##' 
 ##' \tabular{ll}{
 ##' Package: \tab kmmisc\cr
 ##' Type: \tab Package\cr
-##' Version: \tab 0.1-1\cr
-##' Date: \tab 2011-08-13\cr
+##' Version: \tab 0.1-2\cr
+##' Date: \tab 2011-10-06\cr
 ##' License: \tab GPL-2\cr
 ##' LazyLoad: \tab yes\cr
 ##' LazyData: \tab yes\cr
 ##' }
 ##' 
+##' @author Kevin Middleton \email{kmm@@csusb.edu}
+##'
+##' Maintainer: Kevin Middleton \email{kmm@@csusb.edu}
 ##' @name kmmisc-package
-##' 
 ##' @docType package
-##' 
-##' @author Kevin Middleton (kmm@@csusb.edu)
-##' 
-##' Maintainer: Kevin Middleton (kmm@@csusb.edu)
-##'
+##' @title kmmisc
 ##' @keywords package
-##'
-##' 
 NULL
