db57a013ae6a84be112a6acb018a92b828bf0d60
Kevin Middleton <middletonk@missouri.edu>
2015-02-09 08:57:06 -0600

    Update Rd for roxygen2.

--- R/kmmisc.R
+++ R/kmmisc.R
@@ -3,8 +3,8 @@
 ##' \tabular{ll}{
 ##' Package: \tab kmmisc\cr
 ##' Type: \tab Package\cr
-##' Version: \tab 0.1-4\cr
-##' Date: \tab 2013-09-12\cr
+##' Version: \tab 0.1-5\cr
+##' Date: \tab 2015-02-09\cr
 ##' License: \tab GPL-2\cr
 ##' LazyLoad: \tab yes\cr
 ##' LazyData: \tab yes\cr
